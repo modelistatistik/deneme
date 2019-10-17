6d495f979305cab1840ff0e70e14f3c14993b395
Gökhan <52344191+modelistatistik@users.noreply.github.com>
2019-10-17 21:48:19 +0300

    Initial commit

---
+++ R/loadLibrary.R
@@ -0,0 +1,60 @@
+suppressPackageStartupMessages(library("tidyverse"))
+suppressPackageStartupMessages(library("readxl"))
+suppressPackageStartupMessages(library("janitor"))
+suppressPackageStartupMessages(library("report"))
+suppressPackageStartupMessages(library("finalfit"))
+suppressPackageStartupMessages(library("ggstatsplot"))
+
+# citation("tidyverse")
+# citation("readxl")
+# citation("janitor")
+# citation("report")
+
+
+# citation("foreign")
+# citation("tidylog")
+# citation("jmv")
+# citation("tangram")
+# citation("summarytools")
+
+
+# suppressPackageStartupMessages({
+#     library("tidyverse")
+#     library("readxl")
+#     library("janitor")
+#     library("report")
+#     library("finalfit")
+#     library("ggstatsplot")
+#     library("jmv")
+#     library("lubridate")
+#     library("summarytools")
+#     library("survival")
+#     library("psycho")
+#     library("kableExtra")
+# })
+
+
+
+
+
+# citation("tidyverse")
+# citation("readxl")
+# citation("janitor")
+# citation("report")
+
+
+# not used
+# citation("foreign")
+# citation("tidylog")
+# citation("jmv")
+# citation("tangram")
+# citation("summarytools")
+
+# Some Other Packages that can be used
+
+# Packages for Descriptive Statistics
+
+# devtools::install_github("XanderHorn/autoEDA")
+
+
+
